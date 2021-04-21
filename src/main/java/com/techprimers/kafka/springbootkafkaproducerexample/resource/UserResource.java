e77d7f92342a4560870feff8c45c3513440171ad
andreibarroso <andrei.mesquita@globomail.com>
2021-04-21 10:52:32 -0300

    first commit

---
+++ src/main/java/com/techprimers/kafka/springbootkafkaproducerexample/resource/UserResource.java
@@ -0,0 +1,24 @@
+package com.techprimers.kafka.springbootkafkaproducerexample.resource;
+
+import com.techprimers.kafka.springbootkafkaproducerexample.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("kafka")
+public class UserResource {
+
+    @Autowired
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    private static final String TOPIC = "Kafka_Example_json";
+
+    @PostMapping("/publish")
+    public String post(@RequestBody User name) {
+
+        kafkaTemplate.send(TOPIC, name);
+
+        return "Published successfully";
+    }
+}
